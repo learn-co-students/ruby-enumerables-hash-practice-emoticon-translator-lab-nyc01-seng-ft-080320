c95421d07d20095d5c4527ae154064ed4392d95c
Kainan Doneson <kaindoneson@gmail.com>
2020-07-22 23:59:28 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,37 @@
-# require modules here
+require 'pry';
+require 'yaml';
 
-def load_library
-  # code goes here
+def load_library(path)
+  yDS = YAML.load_file(path);
+  emotesHash = {};
+  
+  yDS.each {|emoteName, arrayOfEmotes|
+    emotesHash[emoteName] = {
+      english: arrayOfEmotes[0],
+      japanese: arrayOfEmotes[1]
+    };
+  }
+  return emotesHash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, emote)
+  emoHash = load_library(path);
+  
+  emoHash.each {|name, emoticonHash|
+    if emoticonHash[:english] == emote
+      return emoticonHash[:japanese];
+    end
+  }
+  return 'Sorry, that emoticon was not found'
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(path, emote)
+  emoHash = load_library(path);
+  
+  emoHash.each {|name, emoticonHash|
+    if emoticonHash[:japanese] == emote
+      return name;
+    end
+  }
+  return 'Sorry, that emoticon was not found'
 end
