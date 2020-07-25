445da1906aa19ea08ef0a7ebf30eac4c2c9751a0
Vincent Collis <vinsanity0050@gmail.com>
2020-07-25 01:56:01 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,84 @@
 # require modules here
+require "pry"
+require "yaml"
 
-def load_library
-  # code goes here
+
+file = '/lib/emoticons.yml'
+
+
+def load_library(file)
+  lib = YAML.load_file(file)
+
+  output = lib.each_with_object({}) do |(key, value), final_array| 
+    final_array[key] = {
+      :english => value[0],
+      :japanese => value[1]
+    }
+  end
 end
 
-def get_japanese_emoticon
-  # code goes here
+#############################################
+#############################################
+#############################################
+
+
+
+def get_english_meaning(file, emoji)
+  lib = load_library(file)
+  
+  result = ''
+  # iterate over the hash
+  lib.each do |key, value|
+    if lib[key][:japanese] == emoji
+      result += key
+    end
+  end
+  
+  if result == ''
+    result += "Sorry, that emoticon was not found"
+  end
+  result
 end
 
-def get_english_meaning
-  # code goes here
+# acc[0] = lib[key][:english]
+
+#############################################
+#############################################
+#############################################
+
+def get_japanese_emoticon(file, emoji)
+  lib = load_library(file)
+  
+  result = ''
+  # iterate over the hash
+  lib.each do |key, value|
+    if lib[key][:english] == emoji
+      result += lib[key][:japanese]
+    end
+  end
+  
+  if result == ''
+    result += "Sorry, that emoticon was not found"
+  end
+  result
 end
+
+
+
+# {
+#   "angel"=>["O:)", "☜(⌒▽⌒)☞"],
+# "angry"=>[">:(", "ヽ(ｏ`皿′ｏ)ﾉ"],
+# "bored"=>[":O", "(ΘεΘ;)"],
+# "confused"=>["%)", "(゜.゜)"],
+# "embarrassed"=>[":$", "(#^.^#)"],
+# "fish"=>["><>", ">゜))))彡"],
+# "glasses"=>["8D", "(^0_0^)"],
+# "grinning"=>["=D", "(￣ー￣)"],
+# "happy"=>[":)", "(＾ｖ＾)"],
+# "kiss"=>[":*", "(*^3^)/~☆"],
+# "sad"=>[":'(", "(Ｔ▽Ｔ)"],
+# "surprised"=>[":o", "o_O"],
+# "wink"=>[";)", "(^_-)"]
+# }
+ 
+ 
